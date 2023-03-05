a541d236d2f7fce97248e9800a90891b5235bc43
danakal <dana.kalcikova@centrum.cz>
2023-03-05 13:02:55 +0100

    map helpers

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,17 +1,31 @@
 <template>
-    <button @click="addOne"> Add 10</button>
+    <!--
+    <button @click="increment"> Add 1</button>
+    <button @click="increase({value: 10})"> Add 10</button>
+    -->
+    <button @click="inc"> Add 1</button>
+    <button @click="inr({value: 10})"> Add 10</button>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
     methods: {
-        addOne() {
+        /*addOne() {
            // this.$store.commit('increase', { value: 10 });
            this.$store.dispatch({
             type: 'increase',
             value: 10
            })
-        }
+        } */
+
+        //...mapActions(['increment', 'increase'])
+        ...mapActions({ 
+            inc: 'increment',
+            inr: 'increase'
+        })
+
     }
 }
 </script>
